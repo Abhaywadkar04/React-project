bef8b00abdb5afd3a72042869b25a7675125e49d
Abhaywadkar04 <abhaywadkar2004@gmail.com>
2024-07-02 11:33:29 +0530

    done with input pages

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -1,6 +1,10 @@
 import Header from "./header/Header"
 import Footer from "./footer/Footer"
-
+import container from "./container/Container"
+import Logo from "./Logo"
+import LogoutBtn from "./header/LogoutBtn"
 export{
-    Header,Footer
+    Header,Footer,
+    container,Logo,
+    LogoutBtn
 }
